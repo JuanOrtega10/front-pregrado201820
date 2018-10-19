4cb64305b21e8477dbe9408b88c35c87f082c6fc
rcasalla <rcasalla@uniandes.edu.co>
2018-10-19 09:39:07 -0500

    Mejorar al interceptor del http error

--- src/app/interceptors/httperrorinterceptor.service.ts
+++ src/app/interceptors/httperrorinterceptor.service.ts
@@ -10,8 +10,9 @@ import { catchError } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 import { Injectable } from '@angular/core';
 
+
 @Injectable()
-export class HttpErrorInterceptor implements HttpInterceptor {
+export class HttpErrorInterceptor  extends HttpErrorResponse {
     
     constructor (private toastrService: ToastrService){ }
     
@@ -19,15 +20,21 @@ export class HttpErrorInterceptor implements HttpInterceptor {
         return next.handle(request)
             .pipe(
                 catchError((error: HttpErrorResponse) => {
-                    let errMsg = '';
+                    let errMsg: string = '';
+                    let errorType : string = 'Error';
                     // Client Side Error
                     if (error.error instanceof ErrorEvent) {
-                        errMsg = `Error: ${error.error.message}`;
-                        this.toastrService.error(errMsg, 'Major Error');
+                        errMsg = `Error: ${error.error.message}`;                     
                     }
-                    else {  // Server Side Error
-                        errMsg = `Error Code: ${error.status},  Message: ${error.message}`;
-                        this.toastrService.error(errMsg, 'Major Error');
+                    else {  // Server Side Error                       
+                        if (error.status == 0) {
+                            errMsg = `Error Code: ${error.status},  Message: ${error.statusText} `;    
+                            errorType = 'Major Error';
+                        }                      
+                        else {
+                             errMsg = `Error Code: ${error.status},  Message: ${error.message}`;                             
+                        }                      
+                        this.toastrService.error(errMsg, errorType);
                     }
                     
                     console.log(errMsg);
