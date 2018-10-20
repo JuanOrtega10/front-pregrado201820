09cffbf448bb4116b8c27ba8010b445f1bf489a4
rcasalla <rcasalla@uniandes.edu.co>
2018-10-20 11:31:10 -0500

    eliminating shared ...

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,7 +3,7 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 
 import {AuthorListComponent} from '../author/author-list/author-list.component';
-import {BookListComponent} from '../book/book-list/book-list.component';
+import {BookSidebarComponent} from '../shared/book-sidebar/book-sidebar.component';
 import {EditorialListComponent} from '../editorial/editorial-list/editorial-list.component';
 import { AuthorDetailComponent } from '../author/author-detail/author-detail.component';
 import { BookDetailComponent } from '../book/book-detail/book-detail.component';
@@ -16,7 +16,7 @@ const routes: Routes = [
         children: [
             {
                 path: 'list',
-                component: BookListComponent
+                component: BookSidebarComponent
             },
             {
                 path: ':id',
--- src/app/shared/book-sidebar/book-sidebar.component.ts
+++ src/app/shared/book-sidebar/book-sidebar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Book } from '../../book/book';
-
+import { BookService } from '../../book/book.service';
 @Component({
     selector: 'app-book-sidebar',
     templateUrl: './book-sidebar.component.html',
@@ -17,11 +17,24 @@ export class BookSidebarComponent implements OnInit {
     /**
     * The component's constructor
     */
-    constructor() { }
+    constructor(private bookService: BookService) {  }
+    /**
+    * This method retrieves all the books in the Bookstore to show them in the list
+    */
+    getBooks(): void {
+        this.bookService.getBooks()
+            .subscribe(books => {
+                this.books = books;
+            });
+    }
 
     /**
     * The method which initializes the component
     */
-    ngOnInit() { }
-
+    ngOnInit() {
+    if (this.books == null) {
+     this.getBooks();
+  
+    }
+    }
 }
