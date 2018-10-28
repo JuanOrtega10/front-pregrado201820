13304230be373a8590e15ec4f5876f59b36f6526
rcasalla <rcasalla@uniandes.edu.co>
2018-10-28 16:17:22 -0500

    Cambió  a any el tipo de las fechas y se creó la clase Review

--- src/app/book/book-detail.ts
+++ src/app/book/book-detail.ts
@@ -1,6 +1,6 @@
 import { Author } from '../author/author';
 import { Book } from './book';
-
+import { Review } from './review';
 /**
 * This class represents a book of the BookStore. 
 * It contains all the information relevant to the book.
@@ -9,5 +9,5 @@ export class BookDetail extends Book {
    
     authors: Author[];
     
-    
+    reviews: Review[];
 }
